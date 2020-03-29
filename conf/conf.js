0fd94ae5bc56e2afabcc4c638c1bd1cac156c05d
David Pan <davexpan@gmail.com>
2020-03-29 15:17:25 -0400

    init commit with 3 test cases. (2 Pass, 1 Fail)

---
+++ conf/conf.js
@@ -0,0 +1,27 @@
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+  onPrepare: function() {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+  },
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['../tests/example_spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
